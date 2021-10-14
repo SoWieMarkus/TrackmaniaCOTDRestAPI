73708575e954734b60989e5df8f862b9f4358152
= <=>
2021-10-15 01:06:02 +0200

    removed granady part

--- src/main/java/markus/wieland/tmrestapi/trackmaniarestapi/granady/twitter/Twitter.java
+++
@@ -1,5 +0,0 @@
-package markus.wieland.tmrestapi.trackmaniarestapi.granady.twitter;
-
-public class Twitter {
-    //AAAAAAAAAAAAAAAAAAAAANzVPAEAAAAA3H%2B2Empf0zYxVwdDDYzix4nHOHs%3DrvCSdpVYyfmr82QIzlsCRRZckhSFtEV00mA6jFXAYsWC4BCwTs
-}
